487ce609164da9a8da310e34069004bfd8580320
HUGO BARBOSA <hugohbp1@gmail.com>
2019-10-06 22:36:31 -0300

    First commit

--- utils/Dht11.js
+++
@@ -1,7 +0,0 @@
-class Dht11 {
-  constructor(humidity, temperature) {
-    this.humidity = humidity;
-    this.temperature = temperature;
-  }
-}
-export default Dht11;
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,26 +1,8 @@
 import firebase from "firebase";
+import credencial from "./credencials.json";
 
-const prodConfig = {
-  apiKey: "AIzaSyArOpiXW6HmPvmf_Z8f1dZcXk6BPKAb_X4",
-  authDomain: "iot-remote-home.firebaseapp.com",
-  databaseURL: "https://iot-remote-home.firebaseio.com",
-  projectId: "iot-remote-home",
-  storageBucket: "iot-remote-home.appspot.com",
-  messagingSenderId: "1022390625873",
-  appId: "1:1022390625873:web:8726070771922727347a70"
-};
-
-const devConfig = {
-  apiKey: "AIzaSyArOpiXW6HmPvmf_Z8f1dZcXk6BPKAb_X4",
-  authDomain: "iot-remote-home.firebaseapp.com",
-  databaseURL: "https://iot-remote-home.firebaseio.com",
-  projectId: "iot-remote-home",
-  storageBucket: "iot-remote-home.appspot.com",
-  messagingSenderId: "1022390625873",
-  appId: "1:1022390625873:web:8726070771922727347a70"
-};
-
-//const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+const prodConfig = credencial;
+const devConfig = credencial;
 
 export const firebaseImpl = firebase.initializeApp(prodConfig);
 export const firebaseDatabase = firebase.database();
